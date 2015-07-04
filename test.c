1099be18294947339fd3322cd71d431545361dff
akpil <jyk0802@naver.com>
2015-07-04 09:45:36 +0900

    fix and add

--- test.c
+++ test.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
+int lewis(int a, int b);
+int scarlet(int a);
 int main(void)
 {
+	int number0 = 0; 
+	int number1 = 0;
 	printf ("hello git!\n");
 	printf ("first modify\n");
-	scarlet(3);
+	number0 = scarlet(3);
+	number1 = lewis(3,5);
+	printf ("%d %d\n", number0, number1);
 	return 0;
 }
 
-void scarlet(int a){
+int scarlet(int a){
 	return a;
+}
+
+int lewis(int a, int b)
+{
+	return a+b;
 }
