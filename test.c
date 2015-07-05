ef18042cfa9c4a739d95f1c61ea504d155fd1d84
HanYangERICAjucy <wsy0723@naver.com>
2015-07-05 15:57:39 -0500

    don't know why

--- test.c
+++ test.c
@@ -9,6 +9,7 @@ int main(void)
 	printf ("hello git!\n");
 	printf ("first modify\n");
 	printf ("why without scarlet doing noting?\n");
+	printf ("I don't know why..");
 	number0 = scarlet(3);
 	number1 = lewis(3,5);
 	printf ("%d %d\n", number0, number1);
